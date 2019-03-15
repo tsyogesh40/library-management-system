8ad9c4e480913278e43879c8c2b9a23949fab3fb
Yogesh T S <tsyogesh40@gmail.com>
2019-03-15 11:09:59 +0530

    Update and rename mysql.config.php.sample to mysql.config.php

---
+++ app/config/mysql.config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("MYSQL_HOSTNAME", "sql147.main-hosting.eu");
+define("MYSQL_DATABASE", "u713996426_demo");
+define("MYSQL_USERNAME", "u713996426_demo");
+define("MYSQL_PASSWORD", "demo123");
